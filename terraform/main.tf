1a1cfe675d48d22b114b399833695efb018e4b5d
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-05-25 23:39:06 +0600

    Updated via UDAP

---
+++ terraform/main.tf
@@ -0,0 +1,165 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "< 5.83.0"
+    }
+  }
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "public_key" {
+  description = "SSH public key for EC2 access"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "testweb6deploy"
+}
+
+variable "app_name" {
+  description = "Application name"
+  type        = string
+  default     = "node"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "${var.project_name}-${var.app_name}-key"
+  public_key = var.public_key
+
+  lifecycle {
+    ignore_changes = [public_key]
+  }
+}
+
+resource "aws_security_group" "app_sg" {
+  name        = "${var.project_name}-${var.app_name}-sg"
+  description = "Security group for ${var.project_name} ${var.app_name}"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "App port"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_instance" "app" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.deployer.key_name
+  subnet_id                   = tolist(data.aws_subnets.default.ids)[0]
+  vpc_security_group_ids      = [aws_security_group.app_sg.id]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name    = "${var.project_name}-${var.app_name}"
+    Project = var.project_name
+    App     = var.app_name
+  }
+}
+
+output "instance_id" {
+  description = "EC2 instance ID"
+  value       = aws_instance.app.id
+}
+
+output "public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app.public_ip
+}
+
+output "public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value       = aws_instance.app.public_dns
+}
+
+output "ssh_command" {
+  description = "SSH command to connect to the instance"
+  value       = "ssh -i <private_key> ubuntu@${aws_instance.app.public_ip}"
+}
+
+output "app_url" {
+  description = "Application URL"
+  value       = "http://${aws_instance.app.public_ip}"
+}
